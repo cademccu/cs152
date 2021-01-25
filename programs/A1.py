23d70fa586cd7fcd5eb57c9f7d6b1f925422200e
cademccu <cade21mccumber@gmail.com>
2021-01-25 13:02:55 -0700

    added A1

---
+++ programs/A1.py
@@ -0,0 +1,20 @@
+# print a MadLib using user supplied values for the blanks.
+
+print("name?")
+name = str(input())
+print("adjective?")
+adjective = str(input())
+print("noun?")
+noun = str(input())
+
+print("Please excuse " + name)
+print("who is far too " + adjective)
+print("to attend " + noun + " class.")
+
+
+
+'''
+Please excuse _name_
+who is far too _adjective_
+to attend _noun_ class.
+'''
